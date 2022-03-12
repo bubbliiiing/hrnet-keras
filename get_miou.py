c2490b5280dd6757d894a4fc5e23719d0fb53545
Bubbliiiing <3323290568@qq.com>
2022-03-13 02:42:36 +0800

    update miou

--- get_miou.py
+++ get_miou.py
@@ -3,8 +3,8 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
-from deeplab import DeeplabV3
-from utils.utils_metrics import compute_mIoU
+from hrnet import HRnet_Segmentation
+from utils.utils_metrics import compute_mIoU, show_results
 
 '''
 进行指标评估需要注意以下几点：
@@ -34,27 +34,29 @@ if __name__ == "__main__":
     #-------------------------------------------------------#
     VOCdevkit_path  = 'VOCdevkit'
 
-    image_ids   = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"),'r').read().splitlines() 
-    gt_dir      = os.path.join(VOCdevkit_path, "VOC2007/SegmentationClass/")
-    pred_dir    = "miou_out"
+    image_ids       = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"),'r').read().splitlines() 
+    gt_dir          = os.path.join(VOCdevkit_path, "VOC2007/SegmentationClass/")
+    miou_out_path   = "miou_out"
+    pred_dir        = os.path.join(miou_out_path, 'detection-results')
 
     if miou_mode == 0 or miou_mode == 1:
         if not os.path.exists(pred_dir):
             os.makedirs(pred_dir)
             
         print("Load model.")
-        deeplab = DeeplabV3()
+        hrnet = HRnet_Segmentation()
         print("Load model done.")
 
         print("Get predict result.")
         for image_id in tqdm(image_ids):
             image_path  = os.path.join(VOCdevkit_path, "VOC2007/JPEGImages/"+image_id+".jpg")
             image       = Image.open(image_path)
-            image       = deeplab.get_miou_png(image)
+            image       = hrnet.get_miou_png(image)
             image.save(os.path.join(pred_dir, image_id + ".png"))
         print("Get predict result done.")
 
     if miou_mode == 0 or miou_mode == 2:
         print("Get miou.")
-        compute_mIoU(gt_dir, pred_dir, image_ids, num_classes, name_classes)  # 执行计算mIoU的函数
+        hist, IoUs, PA_Recall, Precision = compute_mIoU(gt_dir, pred_dir, image_ids, num_classes, name_classes)  # 执行计算mIoU的函数
         print("Get miou done.")
+        show_results(miou_out_path, hist, IoUs, PA_Recall, Precision, name_classes)
